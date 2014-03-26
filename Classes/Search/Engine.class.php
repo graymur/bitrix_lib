8de2d3c3b891b9cd95122179c8b0d3d7c1dc3eef
Klim Semenov <klimsemenov86@gmail.com>
2014-03-26 18:31:58 +0200

    область поиска

--- Classes/Search/Engine.class.php
+++ Classes/Search/Engine.class.php
@@ -15,9 +15,11 @@ class Engine
     protected $className = '\Cpeople\Classes\Search\Result';
     protected $modulesList = array();
 
-    public function makeSearch($query, $offset = 0, $limit = 0)
+    public function makeSearch($query, $offset = 0, $limit = 10)
     {
         $retval = array();
+
+        $modulesSQL = $this->makeModulesSQL($this->modulesList);
 
         $sql = "
             SELECT *
@@ -26,6 +28,7 @@ class Engine
                 ON bis.IBLOCK_ID = bsc.PARAM2 AND bis.SITE_ID = '" . SITE_ID . "'
             WHERE
                 (bsc.BODY LIKE '%$query%' OR bsc.TITLE LIKE '%$query%')
+                $modulesSQL
             LIMIT " . (int) $offset . ", " . (int) $limit . "
         ";
 
@@ -35,8 +38,6 @@ class Engine
         {
             $retval[] = new $this->className($row);
         }
-
-        dv($retval);
 
         return $retval;
     }
@@ -55,12 +56,96 @@ class Engine
 
     public function addModule($module, $iblockType = null, $iblockId = null)
     {
-        $this->modulesList[$module][$iblockType][$iblockId] = true;
+        if (is_array($iblockId))
+        {
+            foreach ($iblockId as $id)
+            {
+                $this->modulesList[$module][$iblockType][$id] = true;
+            }
+        } else
+        {
+            $this->modulesList[$module][$iblockType][$iblockId] = true;
+        }
     }
 
     protected function makeQuery($sql)
     {
         global $DB;
         return $DB->Query($sql, false, __LINE__);
+    }
+
+    protected function makeModulesSQL($modulesList)
+    {
+        /** если есть модули */
+        if ($modulesList)
+        {
+            $modulesSQL .= " AND (";
+        }
+
+        foreach ($modulesList as $module => $iblockTypes)
+        {
+            if ($iblockTypes != reset($modulesList))
+            {
+                $modulesSQL .= " OR ";
+            }
+            /** запись модуля */
+            $modulesSQL .= "(bsc.MODULE_ID = '$module'";
+
+            /** если есть типы инфоблоков */
+            if (array_filter(array_keys($iblockTypes)))
+            {
+                $modulesSQL .= " AND (";
+            }
+
+            foreach ($iblockTypes as $iblockType => $iblockIds)
+            {
+                if ($iblockIds != reset($iblockTypes))
+                {
+                    $modulesSQL .= " OR ";
+                }
+                if ($iblockType)
+                {
+                    /** запись типа инфоблока */
+                    $modulesSQL .= "(bsc.PARAM1 = '$iblockType'";
+
+                    if ($iblocks = implode(",", array_keys($iblockIds)))
+                    {
+                        /** запись инфоблоков */
+                        $modulesSQL .= " AND bsc.PARAM2 IN ($iblocks)";
+                    }
+
+                    /** закрываем каждый тип инфоблока */
+                    $modulesSQL .= ")";
+                } elseif ($iblocks = implode(",", array_keys($iblockIds)))
+                {
+                    /** запись инфоблоков */
+                    if (array_filter(array_keys($iblockTypes)))
+                    {
+                        $modulesSQL .= "bsc.PARAM2 IN ($iblocks)";
+                    /** если у нас только инфоблоки без типов */
+                    } else
+                    {
+                        $modulesSQL .= " AND bsc.PARAM2 IN ($iblocks)";
+                    }
+                }
+            }
+
+            /** если есть типы инфоблоков */
+            if (array_filter(array_keys($iblockTypes)))
+            {
+                $modulesSQL .= ")";
+            }
+
+            /** закрываем каждый модуль */
+            $modulesSQL .= ")";
+        }
+
+        /** если есть модули */
+        if ($modulesList)
+        {
+            $modulesSQL .= ")";
+        }
+
+        return $modulesSQL;
     }
 } 
