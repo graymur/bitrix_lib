8f042b8b61a59db09f6392075dcbf875604b2c19
Константин <konstantin.belemuk@gmail.com>
2014-12-08 12:03:48 +0300

    - phpmailer

--- Classes/Forms/Commands/EmailCommand.class.php
+++ Classes/Forms/Commands/EmailCommand.class.php
@@ -14,12 +14,14 @@ use Cpeople\Classes\Forms\Form;
 
 class EmailCommand extends Command
 {
-    private $to;
-    private $from;
-    private $subject;
-    private $body_template;
-    private $data;
-    private $files;
+    protected $mailer;
+
+    protected $to;
+    protected $from;
+    protected $subject;
+    protected $body_template;
+    protected $data;
+    protected $files;
 
     public function __construct($isCritical, $body,Array $to = array(), Array $from = array(), $subject = NULL, Array $files = array())
     {
@@ -29,6 +31,9 @@ class EmailCommand extends Command
         $this->subject       = @coalesce($subject, 'Сообщение на ' . $_SERVER['HTTP_HOST']);
         $this->body_template = $body;
         $this->files         = $files;
+
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/php-mailer/PHPMailerAutoload.php';
+        $this->mailer = new \PHPMailer();
     }
 
     public function execute(Form $form)
@@ -36,25 +41,23 @@ class EmailCommand extends Command
         $this->data = array_change_key_case($form->getData(), CASE_UPPER);
         $body = preg_replace_callback('#{([A-Z_]+)}#i', array($this, 'replaceCallback'), $this->body_template);
 
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/Cpeople/phpmailer/class.phpmailer.php';
-        $mail = new \PHPMailer();
-        $mail->setFrom($this->from[0], $this->from[1]);
-        $mail->addAddress($this->to[0], $this->to[1]);
+        $this->mailer->CharSet = 'UTF-8';
+        $this->mailer->setFrom($this->from[0], $this->from[1]);
+        $this->mailer->addAddress($this->to[0], $this->to[1]);
 
-        $mail->Subject = $this->subject;
-        $mail->msgHTML($body);
+        $this->mailer->Subject = $this->subject;
+        $this->mailer->msgHTML($body);
 
         foreach($this->files as $file)
         {
             if(file_exists($file) && is_readable($file))
             {
-                $mail->addAttachment($file);
+                $this->mailer->addAttachment($file);
             }
         }
 
-        $mail->CharSet = 'utf-8';
 
-        $result = $mail->send();
+        $result = $this->mailer->send();
 
         if(!$result && $this->isCritical)
         {
@@ -62,7 +65,7 @@ class EmailCommand extends Command
         }
         elseif(!$result && !$this->isCritical)
         {
-            $form->setErrors(array($this->getErrorMessage($mail->ErrorInfo)));
+            $form->setErrors(array($this->getErrorMessage($this->mailer->ErrorInfo)));
         }
     }
 
