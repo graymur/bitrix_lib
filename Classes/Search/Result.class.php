65bb8dc85133ffb340a8691fb63689254ef1e1ec
Константин <konstantin.belemuk@gmail.com>
2014-12-22 12:32:08 +0300

    search result _get

--- Classes/Search/Result.class.php
+++ Classes/Search/Result.class.php
@@ -132,4 +132,19 @@ class Result
 
         return $this->element;
     }
+
+    public function __get($name)
+    {
+        if (isset($this->data[strtoupper($name)]))
+        {
+            return $this->data[strtoupper($name)];
+        }
+
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property in __get(): ' . $name .
+            ' in file ' . $trace[0]['file'] .
+            ' line ' . $trace[0]['line'], E_USER_NOTICE
+        );
+    }
 } 
